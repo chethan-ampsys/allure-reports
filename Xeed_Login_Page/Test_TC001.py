f636a80e291fb9873e1b87d2ab330191d63382b7
Chethan K.M <chethanctd168@gmail.com>
2025-06-05 14:40:14 +0530

    Save my local changes before pull

--- Xeed_Login_Page/Test_TC001.py
+++ Xeed_Login_Page/Test_TC001.py
@@ -1,8 +1,10 @@
 import pytest
 import allure
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
@@ -14,36 +16,37 @@ def setup():
     yield driver
     driver.quit()
 
-
 @allure.feature("XEED_Ventures_Login_Page")
 def test_site(setup):
     driver = setup
+    wait = WebDriverWait(driver, 10)  # 10 seconds explicit wait
 
     with allure.step("Open Xeed Ventures Login Page"):
         driver.get("http://157.15.202.244:88/authentication/login")
 
-    with allure.step("Click on username field"):
-        element = driver.find_element(By.XPATH, "//input[@id='username']")
+    with allure.step("Wait for and click on username field"):
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='username']")))
         element.click()
 
     with allure.step("Type in the username"):
         element.send_keys("Rohit")
 
-    with allure.step("Click on password field"):
-        element = driver.find_element(By.XPATH, "//input[@id='password']")
+    with allure.step("Wait for and click on password field"):
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))
+        element.click()
         element.send_keys("Erp@123")
+
+    with allure.step("Wait for and press Login button"):
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Login']")))
         element.click()
 
-    with allure.step("Press Login"):
-        element = driver.find_element(By.XPATH, "//button[normalize-space()='Login']")
-        element.click()
-
+    time.sleep(10)
 
     allure.attach("Test Passed Successfully", name="Test Result", attachment_type=allure.attachment_type.TEXT)
 
-
 # Follow these
 
+# pytest Xeed_Login_Page/Test_TC001.py --alluredir=allure-results
 # pytest test_check.py --alluredir=allure-results
 # allure generate allure-results --clean -o docs
 # git add docs
